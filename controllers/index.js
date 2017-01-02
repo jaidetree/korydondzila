58988518d0bdd6538916b8f63fb778cf63837825
Jay Zawrotny <jayzawrotny@gmail.com>
2017-01-02 16:49:04 -0500

    Starting with production source

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-require("fs").readdirSync("./controllers").forEach(function(file) {
-    var name;
-    if (!(/[a-z]+.js$/g).test(file) && file != "index.js") {
-        return false;
-    }         
-    name = path.basename(file, '.js');
-    module.exports[name] = require("./" + file);
-});
